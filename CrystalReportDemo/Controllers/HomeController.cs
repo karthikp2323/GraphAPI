996a0f063e5e38fe27044f02c36a0f5ab55da50e
Karthik Polina <Karthik.Polina@flhealth.gov>
2020-11-21 15:08:52 -0500

    commit to master

---
+++ CrystalReportDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CrystalReportDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalReportDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        CTLSEntities ctlsEntities = new CTLSEntities();
+        public ActionResult UserList()
+        {
+            var userList = ctlsEntities.Inv_UserSecurities.Where(w => w.Active == true).ToList();
+
+            var userListVM = userList.Select(x => new UserListViewModel()
+            {
+                UserId = x.UserId,
+                UserName = x.UserName,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Email = x.Email,
+                Active = x.Active,
+                LastLogin = x.LastLogin.ToString()
+            }).ToList();
+            return View(userListVM);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
