a5e45bdc9f42f6bf416d680333641392dddebf77
Karthik Polina <Karthik.Polina@flhealth.gov>
2020-11-25 13:27:10 -0500

    after cleanup

---
+++ AzureActiveDirectory/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureActiveDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InviteUser inviteUser = new InviteUser();
+            //inviteUser.AuthenticateAsync().Wait();
+
+            ReadUser readUser = new ReadUser();
+            readUser.GetToken().Wait();
+
+            //Crypto crypto = new Crypto();
+            //crypto.TestEncryption();
+
+            Console.ReadLine();
+        }
+    }
+}
